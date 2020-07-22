8296c0e83c46b80b8d5e0bb2bee785d85e15e4cc
Herb Bowie <hbowie@mac.com>
2020-07-22 12:12:03 -0700

    Added in Leaf

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,12 +1,16 @@
 import Fluent
 import FluentSQLiteDriver
+import Leaf
 import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    app.views.use(.leaf)
+    app.leaf.cache.isEnabled = app.environment.isRelease
+    
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
     app.migrations.add(CreateTodo())
